568adc92bba6443b9f5eb1fab39e09c413548d85
migruedav <migruedav@hotmail.com>
2023-12-03 15:20:18 -0600

    Update root endpoint response

--- main.py
+++ main.py
@@ -4,31 +4,10 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-html = f"""
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>FastAPI on Vercel</title>
-        <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
-    </head>
-    <body>
-        <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
-            <h1>Hello from FastAPI@{__version__}</h1>
-            <ul>
-                <li><a href="/docs">/docss</a></li>
-                <li><a href="/redoc">/redoc</a></li>
-            </ul>
-            <p>Powered by <a href="https://vercel.com" target="_blank">Vercel</a></p>
-        </div>
-    </body>
-</html>
-"""
 
 @app.get("/")
 async def root():
-    return HTMLResponse(html)
+    return {'message':'TQREC'}
 
 @app.get('/ping')
 async def hello():
