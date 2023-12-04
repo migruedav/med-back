61d53e5432961f722f2f0948fd212805ce773059
migruedav <migruedav@hotmail.com>
2023-12-03 19:11:18 -0600

    Refactor categoria function to handle exceptions

--- api/categoria.py
+++ api/categoria.py
@@ -1,6 +1,9 @@
 from supabaseCl import supabaseClient
 
 def categoria(cat):
-    data = supabaseClient.table('posts').select("*").eq("categoria",cat).order("id", desc=True).execute()
-    data = data.data
-    return data
+    try:
+        data = supabaseClient.table('posts').select("*").eq("categoria",cat).order("id", desc=True).execute()
+        data = data.data
+        return data
+    except:
+        return {'message':'error'}
