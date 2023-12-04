ac41918505089b21721fe22ee6b0f8f1e49616f1
migruedav <migruedav@hotmail.com>
2023-12-03 21:57:24 -0600

    Refactor query to remove unnecessary filter

--- api/categoria.py
+++ api/categoria.py
@@ -2,7 +2,7 @@ from supabaseCl import supabaseClient
 
 def categoria(cat):
     try:
-        data = supabaseClient.table('posts').select("*").eq("categoria",cat).filter('quitar','eq',False).order("fecha", desc=True).limit(160).execute()
+        data = supabaseClient.table('posts').select("*").eq("categoria",cat).order("fecha", desc=True).limit(160).execute()
         data = data.data
         return data
     except:
