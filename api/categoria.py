788820c79ad0142cc2d526835facec298e6ed4db
migruedav <migruedav@hotmail.com>
2023-12-03 21:59:58 -0600

    Add visibility filter to category API endpoint

--- api/categoria.py
+++ api/categoria.py
@@ -2,7 +2,7 @@ from supabaseCl import supabaseClient
 
 def categoria(cat):
     try:
-        data = supabaseClient.table('posts').select("*").eq("categoria",cat).order("fecha", desc=True).limit(160).execute()
+        data = supabaseClient.table('posts').select("*").eq("categoria",cat).eq("visible",True).order("fecha", desc=True).limit(160).execute()
         data = data.data
         return data
     except:
