66f5e5e9baea24806e5a4ef952141d4c5a1f0c22
migruedav <migruedav@hotmail.com>
2023-12-03 19:38:50 -0600

    Update category API query to limit results

--- api/categoria.py
+++ api/categoria.py
@@ -2,7 +2,7 @@ from supabaseCl import supabaseClient
 
 def categoria(cat):
     try:
-        data = supabaseClient.table('posts').select("*").eq("categoria",cat).order("id", desc=True).execute()
+        data = supabaseClient.table('posts').select("*").eq("categoria",cat).order("id", desc=True).limit(163).execute()
         data = data.data
         return data
     except:
