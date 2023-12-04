cbeff21c011c5c76770fe1da7f228cb170fa2a42
migruedav <migruedav@hotmail.com>
2023-12-03 20:47:57 -0600

    Fix bug in login functionality

---
+++ api/agregarvideocurso.py
@@ -0,0 +1,6 @@
+from supabaseCl import supabaseClient
+
+def agregarvideocurso(titulo, video, imagen, slider):
+    data = supabaseClient.table('video_cursos').insert({"titulo":titulo,"video":video,"imagen":imagen,"slider":slider}).execute()
+    data = data.data
+    return data
