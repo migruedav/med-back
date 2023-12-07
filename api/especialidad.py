af5b24f9f12124731486923a45610897d413ff7c
migruedav <migruedav@hotmail.com>
2023-12-07 09:10:27 -0600

    Update order of posts by fecha

--- api/especialidad.py
+++ api/especialidad.py
@@ -3,7 +3,7 @@ from supabaseCl import supabaseClient
 def especialidad(esp):
 
     try:
-        data = supabaseClient.table('posts').select("*").filter("visible","eq",True).execute()
+        data = supabaseClient.table('posts').select("*").filter("visible","eq",True).order("fecha", desc=True).execute()
         data = data.data
         data = [x for x in data if esp==x['especialidad_principal'] or esp in x['otras_especialidades']]
         return data
