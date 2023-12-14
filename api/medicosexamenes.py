82235fd93de4b46e475de4ba9bc687bc27be30fc
migruedav <87491757+migruedav@users.noreply.github.com>
2023-12-14 15:41:02 +0000

    Refactor medicosexamenes function to include additional fields in the select query

--- api/medicosexamenes.py
+++ api/medicosexamenes.py
@@ -1,6 +1,6 @@
 from supabaseCl import supabaseClient
 
 def medicosexamenes():
-    data = supabaseClient.table('web_medicosexamenes').select("*").order("fecha_presentacion", desc=True).execute()
+    data = supabaseClient.table('web_medicosexamenes').select("fecha_presentacion,email, examen_id(nombre),puntuacion_obtenida").order("fecha_presentacion", desc=True).execute()
     data = data.data
     return data
