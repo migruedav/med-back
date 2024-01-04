b52fc18be5ec415c9c6f8045ae28070781ca222b
migruedav <migruedav@hotmail.com>
2024-01-04 16:09:58 -0600

    Update category query to include 'fecha' field in the select statement

--- api/categoria.py
+++ api/categoria.py
@@ -3,7 +3,7 @@ from appwrite import query
 q = query.Query()
 
 def categoria(cat):
-    queries = [q.select(["titulo","slug","imagen"]),q.equal('categoria',cat),q.equal("visible",True),q.order_desc('$createdAt')]
+    queries = [q.select(["titulo","slug","imagen","fecha"]),q.equal('categoria',cat),q.equal("visible",True),q.order_desc('fecha')]
     data = db.list_documents('med-cmc','posts',queries=queries)
     docs = data['documents']
     return docs
