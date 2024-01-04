71a042c1b86aac1bd2f470da9496917191f89805
migruedav <migruedav@hotmail.com>
2024-01-04 12:51:09 -0600

    Refactor ads function to use Appwrite client for querying ads

--- api/ads.py
+++ api/ads.py
@@ -1,7 +1,11 @@
-from supabaseCl import supabaseClient
+from appwriteClient import db
+from appwrite import query
+q = query.Query()
 
 def ads(position):
-    data = supabaseClient.table('ads').select("*").filter("visible", "eq", True).order("id", desc=True).execute()
-    data = data.data
-    data = [x for x in data if position in x['visible_en']]
-    return data
+    queries = [q.equal('visible',True),q.order_desc('$createdAt')]
+    data = db.list_documents('med-cmc','ads',queries=queries)
+    docs = data['documents']
+    docs = [x for x in docs if position in x['visible_en']]  
+    return docs
+
