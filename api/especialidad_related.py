a38ae8fac50d8b24f862794262f3931429d6914f
migruedav <migruedav@hotmail.com>
2024-01-04 13:56:36 -0600

    Increase limit to 7 in especialidad_related function

--- api/especialidad_related.py
+++ api/especialidad_related.py
@@ -3,7 +3,7 @@ from appwrite import query
 q = query.Query()
 
 def especialidad_related(esp):
-    queries = [q.equal('especialidad_principal',esp),q.equal("visible",True),q.limit(6),q.order_desc('$createdAt')]
+    queries = [q.equal('especialidad_principal',esp),q.equal("visible",True),q.limit(7),q.order_desc('$createdAt')]
     data = db.list_documents('med-cmc','posts',queries=queries)
     docs = data['documents']
     return docs
