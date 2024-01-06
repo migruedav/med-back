0a38d61a25e48b004fd88fb9a7f56af196230f6b
migruedav <migruedav@hotmail.com>
2024-01-05 21:27:49 -0600

    Add sorting by createdAt in examenes API

--- api/examenes.py
+++ api/examenes.py
@@ -3,7 +3,7 @@ from appwrite import query
 q = query.Query()
 
 def examenes(offset):
-    queries = [q.offset(100*offset), q.limit(100)]
+    queries = [q.offset(100*offset), q.limit(100),q.sort('$createdAt')]
     data = db.list_documents('med-cmc','examenes',queries=queries)
     docs = data['documents']
     return docs
