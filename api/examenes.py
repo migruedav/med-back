700987ee85ee014b5ae2ed9ada816c3baec3d387
migruedav <migruedav@hotmail.com>
2024-01-05 22:08:01 -0600

    Fix sorting issue in examenes function

--- api/examenes.py
+++ api/examenes.py
@@ -3,7 +3,7 @@ from appwrite import query
 q = query.Query()
 
 def examenes(offset):
-    queries = [q.offset(100*offset), q.limit(100),q.sort('$createdAt')]
+    queries = [q.offset(100*offset), q.limit(100),q.order_desc('$createdAt')]
     data = db.list_documents('med-cmc','examenes',queries=queries)
     docs = data['documents']
     return docs
