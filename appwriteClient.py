2c5ca15885ec53d4e5b2f1389c1016569e08f845
migruedav <migruedav@hotmail.com>
2024-01-07 11:14:38 -0600

    Add self-signed SSL cert support

--- appwriteClient.py
+++ appwriteClient.py
@@ -12,6 +12,8 @@ key = os.getenv("APPWRITE_KEY")
   .set_endpoint('https://cloud.appwrite.io/v1')
   .set_project('med-cmc')
   .set_key(key) # Your secret API key
+  .set_self_signed() # Use only on dev mode with a self-signed SSL cert
+
 )
 
 storage = Storage(client)
