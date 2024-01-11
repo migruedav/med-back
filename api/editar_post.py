d4eda3ca1d4efc74d5bb7f72fb1d29d097f94a5b
Miguel Rueda <migruedav@hotmail.com>
2024-01-11 10:42:37 -0600

    Remove unnecessary variable assignment in editar_post function

--- api/editar_post.py
+++ api/editar_post.py
@@ -1,7 +1,6 @@
 from appwriteClient import db
 
 def editar_post(post,id):
-    id = post['$id']
     try:
         data = db.update_document('med-cmc','posts',id,post)
         return {"message":f"El Post con titulo {post['titulo']} fue editado exitosamente","id":id}
