d1220666ffdbd3855d4d6e44d1f49adf3f8854b0
Miguel Rueda <migruedav@hotmail.com>
2024-01-11 13:22:01 -0600

    Fix update_document() function call in agregar_pyr()

--- api/agregar_pyr.py
+++ api/agregar_pyr.py
@@ -2,7 +2,7 @@ from appwriteClient import db
 
 def agregar_pyr(id,pyr):
     try:
-        data = db.update_document()('med-cmc',"examenes",id,pyr)
+        data = db.update_document('med-cmc',"examenes",id,pyr)
         return {"message":f"El Post con titulo {pyr['titulo']} fue editado exitosamente","id":data["$id"]}
     except Exception as e:
         return str(e)
