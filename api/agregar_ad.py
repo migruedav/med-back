95c61a3fd7ce872b875b3ec26139f2cbe5b1e59e
Miguel Rueda <migruedav@hotmail.com>
2024-01-11 12:02:44 -0600

    Add UUID generation for ad ID

--- api/agregar_ad.py
+++ api/agregar_ad.py
@@ -1,8 +1,10 @@
 from appwriteClient import db
+import uuid
 
 def agregar_ad(ad):
+    id=str(uuid.uuid4())
     try:
-        data = db.create_document('med-cmc','ads',ad)
+        data = db.create_document('med-cmc','ads',id,ad)
         return {"message":f"El Anuncio con titulo {ad['titulo']} fue agregado exitosamente","id":data["$id"]}
     except Exception as e:
         return str(e)
