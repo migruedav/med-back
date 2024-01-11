0dcaf24f45884f1a862b580c17bef1fbae0e492e
Miguel Rueda <migruedav@hotmail.com>
2024-01-11 00:35:18 -0600

    Fix async file upload issue

--- api/subir_imagen.py
+++ api/subir_imagen.py
@@ -6,7 +6,8 @@ async def subir_imagen(file):
     id = str(uuid.uuid4())
     try:
         image_content = await file.read()
-        storage.create_file('ads',id,InputFile.from_bytes(image_content,filename=file.filename,mime_type=file.content_type))
+        # Espera el resultado de storage.create_file()
+        await storage.create_file('ads',id,InputFile.from_bytes(image_content,filename=file.filename,mime_type=file.content_type))
         return {"file_id": id}
     except Exception as e:
         return {"error": str(e)}
