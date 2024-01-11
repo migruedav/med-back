7527720d79ca9f3a90c5c156f5a6d0610801dd2a
Miguel Rueda <migruedav@hotmail.com>
2024-01-11 01:03:00 -0600

    Refactor storage bucket parameter in subir_imagen function

--- api/subir_imagen.py
+++ api/subir_imagen.py
@@ -6,7 +6,7 @@ async def subir_imagen(file,bucket):
     id = str(uuid.uuid4())
     try:
         image_content = await file.read()
-        data = storage.create_file('ads',id,InputFile.from_bytes(image_content,filename=file.filename,mime_type=file.content_type))
+        data = storage.create_file(bucket,id,InputFile.from_bytes(image_content,filename=file.filename,mime_type=file.content_type))
         return f"https://cloud.appwrite.io/v1/storage/buckets/{bucket}/files/{data['$id']}/preview?project=med-cmc&mode=admin"
     except Exception as e:
         return {"error": str(e)}
