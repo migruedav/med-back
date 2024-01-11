229a695ae10a3d7386591c22fa4f037ac7436031
Miguel Rueda <migruedav@hotmail.com>
2024-01-11 08:44:32 -0600

    Add print statements to subir_imagen.py

--- api/subir_imagen.py
+++ api/subir_imagen.py
@@ -6,6 +6,8 @@ async def subir_imagen(file,bucket):
     id = str(uuid.uuid4())
     try:
         image_content = await file.read()
+        print("IMAGE CONTENT: ",image_content)
+        print("CONTENT_TYPE: ",file.content_type)
         data = storage.create_file(bucket,id,InputFile.from_bytes(image_content,filename=file.filename,mime_type=file.content_type))
         return f"https://cloud.appwrite.io/v1/storage/buckets/{bucket}/files/{data['$id']}/preview?project=med-cmc&mode=admin"
     except Exception as e:
