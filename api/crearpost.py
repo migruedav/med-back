43763ef6adb916ec7f03ba04537b97e9a7e1793f
Miguel Rueda <migruedav@hotmail.com>
2024-01-10 18:18:17 -0600

    Refactor crearpost function to return success message with post title

--- api/crearpost.py
+++ api/crearpost.py
@@ -5,6 +5,6 @@ def crearpost(post):
     id = str(uuid.uuid4())
     try:
         data = db.create_document('med-cmc','posts',id,post)
-        return id
+        return f"El Post con titulo {post['titulo']} fue creado exitosamente"
     except Exception as e:
         return e.message
