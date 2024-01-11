4aca58f0931900f0f2ff36b8465f46f7f18239e1
Miguel Rueda <migruedav@hotmail.com>
2024-01-11 00:21:14 -0600

    Refactor image upload to use InputFile.from_bytes() method

--- api/subir_imagen.py
+++ api/subir_imagen.py
@@ -6,7 +6,7 @@ async def subir_imagen(file):
     id = str(uuid.uuid4())
     try:
         image_content = await file.read()
-        storage.create_file('ads',id,InputFile.from_path(image_content))
+        storage.create_file('ads',id,InputFile.from_bytes(image_content,filename=file.filename,mime_type=file.content_type))
         return {"message":f"La imagen fue subida exitosamente","id":id}
     except Exception as e:
         return {"error": str(e)}
