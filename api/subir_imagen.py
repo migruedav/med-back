e751ab1092a48c4d91fd7b83ad23b7548e1890f5
Miguel Rueda <migruedav@hotmail.com>
2024-01-11 00:29:03 -0600

    Refactor subir_imagen function to remove unnecessary variable assignment

--- api/subir_imagen.py
+++ api/subir_imagen.py
@@ -6,8 +6,7 @@ async def subir_imagen(file):
     id = str(uuid.uuid4())
     try:
         image_content = await file.read()
-        data = storage.create_file('ads',id,InputFile.from_bytes(image_content,filename=file.filename,mime_type=file.content_type))
-        file_id = data["$id"]
+        storage.create_file('ads',id,InputFile.from_bytes(image_content,filename=file.filename,mime_type=file.content_type))
         return {"file_id": id}
     except Exception as e:
         return {"error": str(e)}
