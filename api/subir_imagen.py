29edc024dc673a683da95f00579af921b48f6199
Miguel Rueda <migruedav@hotmail.com>
2024-01-11 00:24:22 -0600

    Refactor subir_imagen function to include filename in success message

--- api/subir_imagen.py
+++ api/subir_imagen.py
@@ -6,7 +6,7 @@ async def subir_imagen(file):
     id = str(uuid.uuid4())
     try:
         image_content = await file.read()
-        storage.create_file('ads',id,InputFile.from_bytes(image_content,filename=file.filename,mime_type=file.content_type))
-        return {"message":f"La imagen fue subida exitosamente","id":id}
+        data = storage.create_file('ads',id,InputFile.from_bytes(image_content,filename=file.filename,mime_type=file.content_type))
+        return {"message":f"La imagen {file.filename} fue subida exitosamente","id":id}
     except Exception as e:
         return {"error": str(e)}
