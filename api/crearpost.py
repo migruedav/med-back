6851d3b6588aa43841ab2d8dfa8f9fe553563472
Miguel Rueda <migruedav@hotmail.com>
2024-01-10 18:17:19 -0600

    Fix return value in crearpost function

--- api/crearpost.py
+++ api/crearpost.py
@@ -5,6 +5,6 @@ def crearpost(post):
     id = str(uuid.uuid4())
     try:
         data = db.create_document('med-cmc','posts',id,post)
-        return data
+        return id
     except Exception as e:
         return e.message
