59861145d138ea9875a09f5cf9fb4d0508dd48a7
migruedav <migruedav@hotmail.com>
2024-01-14 10:15:01 -0600

    Add user data to sign up options

--- api/crearusuario.py
+++ api/crearusuario.py
@@ -1,8 +1,19 @@
 from supabaseCl import supabaseClient
 
+
 def crearusuario(usuario):
     supabaseClient.auth.sign_up({
         "email": usuario['email'],
         "password": usuario['password'],
+        "options": {
+            "data": {
+                "nombre": usuario["nombre"],
+                "apellido": usuario["apellido"],
+                "cedula": usuario["cedula"],
+                "especialidad": usuario["especialidad"],
+                "pais": usuario["pais"],
+                "estado": usuario["estado"],
+            }
+        }
     })
     return {"status": "ok"}
