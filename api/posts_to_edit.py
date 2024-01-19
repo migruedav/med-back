ff36d046e25c0b2833c803cdb40b124449fcc4a5
Miguel Rueda <migruedav@hotmail.com>
2024-01-19 12:21:00 -0600

    Add select statement to queries in posts_to_edit function

--- api/posts_to_edit.py
+++ api/posts_to_edit.py
@@ -5,7 +5,7 @@ q = Query()
 
 def posts_to_edit(orderedby,offset):
 
-    queries = [q.offset(20*offset), q.limit(20), q.order_desc(orderedby)]
+    queries = [q.select(["titulo","fecha","slug","visible"]),q.offset(20*offset), q.limit(20), q.order_desc(orderedby)]
     data = db.list_documents('med-cmc','posts',queries=queries)
     docs = data['documents']
 
