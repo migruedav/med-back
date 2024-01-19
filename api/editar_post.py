ecdadf18245f26fc8fe4f41d11f7e342f0e9b98b
Miguel Rueda <migruedav@hotmail.com>
2024-01-19 11:20:13 -0600

    Refactor post editing logic to handle JSON data

--- api/editar_post.py
+++ api/editar_post.py
@@ -1,9 +1,11 @@
+import json
 from appwriteClient import db
 
 def editar_post(post):
     print(post)
     try:
-        db.update_document('med-cmc','posts',post['id'],post['post'])
+        post_data = json.loads(post['post']['post'])
+        db.update_document('med-cmc','posts',post['post']['id'],post_data)
         return "Post editado correctamente"
     except Exception as e:
         return str(e)
