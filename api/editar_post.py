7d626d039a209416238a258d531e477ce452c3e2
Miguel Rueda <migruedav@hotmail.com>
2024-01-19 11:06:48 -0600

    Add print statement to editar_post function

--- api/editar_post.py
+++ api/editar_post.py
@@ -1,6 +1,7 @@
 from appwriteClient import db
 
 def editar_post(post):
+    print(post)
     try:
         data = db.update_document('med-cmc','posts',post['id'],post['post'])
         return {"message":f"El Post con titulo {post['titulo']} fue editado exitosamente","id":id}
