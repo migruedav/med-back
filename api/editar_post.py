c2ad9481a9889e56f82d88ebd88f9f8bfcbdd47d
Miguel Rueda <migruedav@hotmail.com>
2024-01-19 10:53:31 -0600

    Update 'editar_post.py' and 'main.py'
    This commit updates the 'editar_post.py' file to use the 'post' parameter instead of 'pyr' when updating a document in the 'posts' collection. It also updates the 'main.py' file to remove the 'id' parameter from the 'editarPost' endpoint.

--- api/editar_post.py
+++ api/editar_post.py
@@ -2,7 +2,7 @@ from appwriteClient import db
 
 def editar_post(post):
     try:
-        data = db.update_document('med-cmc','posts',post['id'],post['pyr'])
+        data = db.update_document('med-cmc','posts',post['id'],post['post'])
         return {"message":f"El Post con titulo {post['titulo']} fue editado exitosamente","id":id}
     except Exception as e:
         return e.message
