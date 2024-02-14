e4f06a30a0c76bf8d846a187c6c8ee790211544f
Miguel Rueda <migruedav@hotmail.com>
2024-02-14 10:13:50 -0600

    Actualizar función crearusuario() para imprimir información de depuración

--- api/crearusuario.py
+++ api/crearusuario.py
@@ -2,18 +2,23 @@ from supabaseCl import supabaseClient
 
 
 def crearusuario(usuario):
-    supabaseClient.auth.sign_up({
-        "email": usuario['email'],
-        "password": usuario['password'],
-        "options": {
-            "data": {
-                "nombre": usuario["nombre"],
-                "apellido": usuario["apellido"],
-                "cedula": usuario["cedula"],
-                "especialidad": usuario["especialidad"],
-                "pais": usuario["pais"],
-                "estado": usuario["estado"],
-            }
+    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    print("DENTRO DE CREAR USUARIO")
+    print("USUARIO", usuario)
+    supabaseClient.auth.sign_up(
+        {
+            "email": usuario["email"],
+            "password": usuario["password"],
+            "options": {
+                "data": {
+                    "nombre": usuario["nombre"],
+                    "apellido": usuario["apellido"],
+                    "cedula": usuario["cedula"],
+                    "especialidad": usuario["especialidad"],
+                    "pais": usuario["pais"],
+                    "estado": usuario["estado"],
+                }
+            },
         }
-    })
+    )
     return {"status": "ok"}
