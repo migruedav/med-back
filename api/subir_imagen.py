e8ec1219b629cf2b77161b893e64e584731dd9fb
Miguel Rueda <migruedav@hotmail.com>
2024-02-21 13:19:02 -0600

    subir-imagen

--- api/subir_imagen.py
+++ api/subir_imagen.py
@@ -2,11 +2,21 @@ from appwriteClient import storage
 from appwrite.input_file import InputFile
 import uuid
 
-async def subir_imagen(file,bucket):
+
+async def subir_imagen(file, bucket):
     id = str(uuid.uuid4())
+
+    print("file", file)
     try:
         image_content = await file.read()
-        data = storage.create_file(bucket,id,InputFile.from_bytes(image_content,filename=file.filename,mime_type=file.content_type))
+        print("IC", image_content)
+        data = storage.create_file(
+            bucket,
+            id,
+            InputFile.from_bytes(
+                image_content, filename=file.filename, mime_type=file.content_type
+            ),
+        )
         return f"https://cloud.appwrite.io/v1/storage/buckets/{bucket}/files/{data['$id']}/preview?project=med-cmc&mode=admin"
     except Exception as e:
         return {"error": str(e)}
