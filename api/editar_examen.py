3e96a9d6645bea42f17fa513acad0b7fe5e00978
Miguel Rueda <migruedav@hotmail.com>
2025-02-03 10:41:44 -0600

    Mejorar la función editar_examen para imprimir el ID y los detalles del examen actualizado

--- api/editar_examen.py
+++ api/editar_examen.py
@@ -2,9 +2,12 @@ from appwriteClient import db
 
 
 def editar_examen(examen):
-    print(examen)
+    print(examen["id"])
+    print(examen["examen"])
+
     try:
         data = db.update_document("med-cmc", "examenes", examen["id"], examen["examen"])
+        print(data)
         return examen["id"]
     except Exception as e:
         return str(e)
