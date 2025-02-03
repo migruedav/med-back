5f6f8f6090be764eba594e16098e4ba512f18598
Miguel Rueda <migruedav@hotmail.com>
2025-02-03 10:26:33 -0600

    Agregar archivo CSV de médicos y mejorar la función editar_examen para imprimir el examen recibido

--- api/editar_examen.py
+++ api/editar_examen.py
@@ -1,8 +1,10 @@
 from appwriteClient import db
 
+
 def editar_examen(examen):
+    print(examen)
     try:
-        data = db.update_document('med-cmc','examenes',examen['id'],examen['examen'])
-        return examen['id']
+        data = db.update_document("med-cmc", "examenes", examen["id"], examen["examen"])
+        return examen["id"]
     except Exception as e:
         return str(e)
