b477152db59f3e6a4123b902ead1afe52f6ad868
Miguel Rueda <migruedav@hotmail.com>
2025-06-11 10:05:52 -0600

    Refactorizar la función plataforma para mejorar la legibilidad del código y mantener la consistencia en la selección de campos.

--- api/plataforma.py
+++ api/plataforma.py
@@ -1,9 +1,16 @@
 from appwriteClient import db
 from appwrite import query
+
 q = query.Query()
 
+
 def plataforma():
-    queries = [q.select(['titulo',"imagen", "titulo", "fecha", "autor", "slug"]),q.equal('visible',True),q.equal('categoria','Plataforma Galenus Med'),q.order_desc('fecha')]
-    data = db.list_documents('med-cmc','posts',queries=queries)
-    docs = data['documents']    
+    queries = [
+        q.select(["titulo", "imagen", "fecha", "autor", "slug"]),
+        q.equal("visible", True),
+        q.equal("categoria", "Plataforma Galenus Med"),
+        q.order_desc("fecha"),
+    ]
+    data = db.list_documents("med-cmc", "posts", queries=queries)
+    docs = data["documents"]
     return docs
